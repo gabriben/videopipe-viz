b6b5736e7b92d433d6fee828633fe22475c9086e
caskok <caskok1995@gmail.com>
2023-08-29 01:23:57 +0200

    Changed and added examples

---
+++ example_face.py
@@ -0,0 +1,7 @@
+import videopipeViz
+
+videopipeViz.textDetection(json_path='JSON/',
+                           video_path='Videos/',
+                           v_name='HIGH_LIGHTS_I_SNOWMAGAZINE_I_SANDER_26',
+                           out_path='',
+                           json_postfix='_frame_text_detection_datamodel')
