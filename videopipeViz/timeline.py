f7ab67c9f672fcfe88775bc0908a9f0ccdcd9afd
caskok <caskok1995@gmail.com>
2023-08-28 22:17:10 +0200

    Refactor _calc_time_indicator and added docstrings

--- videopipeViz/timeline.py
+++ videopipeViz/timeline.py
@@ -1,5 +1,4 @@
 import subprocess
-import pandas as pd
 import numpy as np
 import moviepy.editor as mp
 import matplotlib.pyplot as plt
@@ -9,6 +8,7 @@ import videopipeViz.core_viz as core
 
 
 class TimelineAnimation:
+    """ Class for the timeline animation. """
     def __init__(self,
                  clip,
                  v_name,
@@ -36,22 +36,45 @@ class TimelineAnimation:
         self.fig, self.ax = self._make_timeline()
         self.total_video_time = clip.duration + len(data) * detection_delay_sec
 
-    def _calculate_time_indicator_frame_number(self, t):
-        current_frame = int(round(t * self.fps))
-        if self.delay_frames_left > 0:
-            if self.delay_frames_left == 0:
-                self.delay_frames_left = int(self.detection_delay_sec * self.fps)
-            elif self.delay_frames_left < int(self.detection_delay_sec * self.fps):
-                self.delay_frames_left -= 1
-                self.total_frames_delay += 1
-            elif current_frame - self.total_frames_delay in self.delay_frames_set:
-                self.delay_frames_set.discard(current_frame - self.total_frames_delay)
-                self.delay_frames_left -= 1
-                self.total_frames_delay += 1
-            return current_frame - self.total_frames_delay + 1
-        return current_frame
+    def _calculate_time_indicator_frame_number(self, t: int) -> int:
+        """ Helper function to calculate the frame number of the video when
+        frames are delayed (frozen). This is necessary for the insertion of
+        the freeze frames of shotboundaries.
 
-    def _make_timeline(self, height_ratio=7, detail_modifier=1.0):
+        Args:
+            t (int): the current timestamp.
+
+        Returns:
+            int: current frame number.
+        """
+        frame_number = int(round(t * self.fps))
+
+        if self.delay_frames_left > 0:
+            self.delay_frames_left -= 1
+            self.total_frames_delay += 1
+
+            if frame_number - self.total_frames_delay in self.delay_frames_set:
+                self.delay_frames_set.discard(frame_number -
+                                              self.total_frames_delay)
+                return frame_number - self.total_frames_delay + 1
+
+        return frame_number
+
+    def _make_timeline(self,
+                       height_ratio: int = 7,
+                       detail_modifier: float = 1.0):
+        """ Makes the timeline plot.
+
+        Args:
+            height_ratio (int, optional): The ratio of the video to the
+            timeline. Defaults to 7 for a timeline that's 1/7 the video heigth.
+            detail_modifier (float, optional): Modifier for the amount of
+            detail of the timeline plot. Higher number equals more detail.
+            Defaults to 1.0.
+
+        Returns:
+            fig, ax: The figure and axis of the plot.
+        """
         DETAIL_SWEETSPOT = 0.03
 
         w, h = self.clip.size
@@ -93,7 +116,16 @@ class TimelineAnimation:
         plt.tight_layout()
         return fig, ax
 
-    def _make_frame(self, t):
+    def _make_frame(self, t: int):
+        """ Helper function to make the timeline animation. Determines the
+        the frame of the animation for all timestamps t.
+
+        Args:
+            t (int): current timestamp
+
+        Returns:
+            frame image
+        """
         if self.last_line is not None:
             self.last_line.remove()
 
@@ -106,6 +138,21 @@ class TimelineAnimation:
         return mplfig_to_npimage(self.fig)
 
     def create(self, timeline_postfix='_timeline_only'):
+        """ Create the timeline animation and write to a file formatted as:
+        <original video name> + <task> + <timeline_postfix> + '.mp4'
+
+        example: 'video_name_face_detection_timeline_only.mp4'
+
+        Also saves this filename for when the add_to_video method will be
+        called afterwards.
+
+        Args:
+            timeline_postfix (str, optional): postfix of the filename to
+            write to. Defaults to '_timeline_only'.
+
+        Returns:
+            self
+        """
         animation = mp.VideoClip(self._make_frame,
                                  duration=self.total_video_time)
         core.write_clip(animation,
@@ -115,11 +162,23 @@ class TimelineAnimation:
 
         return self
 
-    def add_to_video(self, video_path, output_filename):
+    def add_to_video(self, burned_in_video_path: str, output_filename: str):
+        """ Adds the timeline animation to the video with
+        the burned in detections.
+
+        Args:
+            burned_in_video_path (str): path of the video with the detections
+            burned into it.
+            output_filename (str): filename of the output video.
+
+        Raises:
+            ValueError: If self.file_name is not set by the create method, an
+            error is raised.
+        """
         if not self.file_name:
             raise ValueError('No filename known, ' +
                              'did you create the animation first?')
-        cmd = f"ffmpeg -i {video_path} -i {self.file_name} " \
+        cmd = f"ffmpeg -i {burned_in_video_path} -i {self.file_name} " \
               f"-filter_complex vstack {output_filename}"
         subprocess.call(cmd, shell=True)
 
