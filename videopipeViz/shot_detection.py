7fc0e9100de4a89e42402b3ef50d236e5588d85c
caskok <caskok1995@gmail.com>
2023-08-29 13:54:04 +0200

    Working shot detection

--- videopipeViz/shot_detection.py
+++ videopipeViz/shot_detection.py
@@ -1,32 +1,34 @@
+import os
 import pandas as pd
 import numpy as np
 import moviepy.editor as mp
 from PIL import Image
-from midroll_marker import make_frame_line
-
+from videopipeViz.midroll_marker import make_frame_line
 import videopipeViz.core_viz as core
+import videopipeViz.timeline as tl
 
 
-def read_shot_detection(v_name, task):
+def read_shot_detection(json_path, v_name, json_postfix):
     '''
     Read the JSON file with the shot detection data.
     '''
-    shots = pd.read_json(v_name + '/' + v_name + task + '.json',
+    shots = pd.read_json(json_path + v_name + '/' +
+                         v_name + json_postfix + '.json',
                          lines=True)
     shots_detected = [f for f in shots.data[0]]
     return shots_detected
 
 
-def create_shot_clip(shot_end_frame_number, size, shot_frame_duration=1):
+def create_shot_clip(clip, shot_end_frame_number, shot_frame_duration=1):
     '''
-    Create frame of length txtclip_dur with txt in the center.
+    Create frame of length shotclip_dur with txt in the center.
     '''
-    w, h = size
-    shot_info_frame = Image.new('RGB', size)
+    _, h = clip.size
+    shot_info_frame = Image.new('RGB', clip.size)
     frame_line = make_frame_line(clip,
                                  shot_end_frame_number,
                                  frame_type="Shot ends")
-    frame_line.thumbnail(size, Image.LANCZOS)
+    frame_line.thumbnail(clip.size, Image.LANCZOS)
     shot_info_frame.paste(frame_line, (0, int(h/2)))
     shotclip = mp.ImageClip(np.asarray(shot_info_frame),
                             duration=shot_frame_duration)
@@ -34,16 +36,20 @@ def create_shot_clip(shot_end_frame_number, size, shot_frame_duration=1):
     return shotclip
 
 
-def get_shot_clips(clip, shots_detected, shot_frame_duration=1, shot_count=1):
+def get_shot_clips(clip,
+                   shots_detected,
+                   shot_frame_duration=3,
+                   timestamp_offset=0):
     '''
     Returns a list of clips with the shot boundaries and the shots themselves.
     shots_limit determines how many shot boundaries are included.
     '''
     clips = []
+    frame_duration = 1 / clip.fps
     for shot in shots_detected:
         shot_boundary_frame_number = shot['dimension_idx']
-        shotclip = create_shot_clip(shot_boundary_frame_number,
-                                    clip.size,
+        shotclip = create_shot_clip(clip,
+                                    shot_boundary_frame_number,
                                     shot_frame_duration=shot_frame_duration)
         timestamp = shot_boundary_frame_number * frame_duration
 
@@ -55,58 +61,85 @@ def get_shot_clips(clip, shots_detected, shot_frame_duration=1, shot_count=1):
         clips.append(shotclip)
         clips.append(subclip)
 
-    return clips, timestamp, shot_count
+    return clips, timestamp
 
 
-if __name__ == '__main__':
-    # this can be empty if the video file and its videopipe output
-    # are at the same location as the code.
-    video_path = 'Videos/'
-    v_name = 'HIGH_LIGHTS_I_SNOWMAGAZINE_I_SANDER_26'
-    task = '_shot_boundaries_datamodel'
-    RESIZE_DIM = 640
-    output_filename = v_name + task + '_2sec.mp4'
+def shotDetection(json_path: str,
+                  video_path: str,
+                  v_name: str,
+                  out_path: str,
+                  json_postfix: str = '_shot_boundaries_datamodel',
+                  add_timeline=True,
+                  add_tl_indicators=True,
+                  add_tl_graph=True,
+                  frames_per_round: int = 100) -> None:
+    """ Burns in the shot detection JSON in the video and
+    adds a timeline animation on the bottom displaying the detection density.
 
-    shots_detected = read_shot_detection(v_name, task)
+    Args:
+        json_path (str): path of the JSON folder
+        video_path (str): path of the folder of the video.
+        v_name (str): name of the original video.
+        out_path (str): folder for the output video.
+        json_postfix (str, optional): postfix of the JSON file.
+                                      Defaults to '_shot_detection_datamodel'.
+        add_timeline (bool, optional): Flag for the addition of the timeline.
+                                       Defaults to True.
+        add_tl_indicators (bool, optional): Flag for the timeline indicators.
+                                            Defaults to True.
+        add_tl_graph (bool, optional): Flag for the addition of the graphline.
+                                       Currently UNUSED. Defaults to True.
+        frames_per_round (int, optional): Sets the amount of detections per
+        round, can be optimized for performance. Defaults to 100.
+    """
+    shot_detected = read_shot_detection(json_path, v_name, json_postfix)
+    clip = core.read_clip(video_path + v_name)
 
-    v_name = video_path + v_name
+    shot_frame_duration = 3
+    total_rounds = len(shot_detected) // frames_per_round
 
-    clip = core.read_clip(v_name)
-    fps = clip.fps
-    frame_duration = 1/fps
-    w, h = clip.size
-
-    shots_per_round = 20
-    shot_frame_duration = 2
     prev_ts = 0
-    shot_count = 1
+    with open(out_path + 'shot_detection.txt', 'w') as f:
+        for round in range(total_rounds + 1):
+            clips = []
+            start_shot_number = round * frames_per_round
+            end_shot_number = start_shot_number + frames_per_round
+            shot_batch = shot_detected[start_shot_number:end_shot_number]
+            clips, prev_ts = get_shot_clips(clip,
+                                            shot_batch,
+                                            shot_frame_duration,
+                                            prev_ts)
 
-    f = open('shot_detection.txt', 'w')
+            if (round == total_rounds):
+                clips.append(clip.subclip(prev_ts, clip.duration))
 
-    # Get shot clips and write them to file. Then concatenate the files.
-    for round in range(len(shots_detected) // shots_per_round + 1):
-        clips = []
-        start_shot_number = round * shots_per_round
-        end_shot_number = start_shot_number + shots_per_round
-        shot_batch = shots_detected[start_shot_number:end_shot_number]
-        clips, prev_t, shot_count = get_shot_clips(clip,
-                                                   shot_batch,
-                                                   shot_frame_duration=shot_frame_duration,
-                                                   shot_count=shot_count)
+            core.write_clip(mp.concatenate_videoclips(clips),
+                            v_name,
+                            postfix=str(round),
+                            audio=False)
+            f.write('file ' + v_name + '_' + str(round) + '.mp4\n')
 
-        if (round == len(shots_detected) // shots_per_round):
-            clips.append(clip.subclip(prev_t, clip.duration))
-
-        core.write_clip(mp.concatenate_videoclips(clips),
-                        v_name,
-                        str(round),
-                        True)
-        f.write('file ' + v_name + '_' + str(round) + '.mp4\n')
-    f.close()
+    new_v_name = out_path + v_name + json_postfix
 
     core.files_to_video(clip,
                         v_name,
-                        round,
-                        'shot_detection.txt',
-                        output_filename,
-                        False)
+                        total_rounds,
+                        out_path + 'shot_detection.txt',
+                        new_v_name + '.mp4')
+
+    if not add_timeline:
+        return
+
+    shot_frame_numbers = [shot['dimension_idx'] for shot in shot_detected]
+    timeline = tl.TimelineAnimation(clip,
+                                    v_name,
+                                    shot_frame_numbers,
+                                    add_graph=add_tl_graph,
+                                    add_indicators=add_tl_indicators,
+                                    detection_delay_sec=0,
+                                    task=json_postfix)
+
+    timeline.add_to_video(new_v_name + '.mp4', new_v_name + '_timeline.mp4')
+
+    if os.path.exists(new_v_name + '.mp4'):
+        os.remove(new_v_name + '.mp4')
